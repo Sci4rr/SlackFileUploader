fc54e8076bb1142d5cd1c3272b5047ed37e5c014
Sci4rr <163331503+Sci4rr@users.noreply.github.com>
2024-06-19 00:40:38 +0200

    cleaning code

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
 import axios from 'axios';
+
 const fileInput = document.querySelector('#file-input');
 const uploadButton = document.querySelector('#upload-button');
-const statusElement = document.querySelector('#query');
+const statusElement = document.querySelector('#status');
+
 function updateStatus(message, isError = false) {
-  statusElement.textContent = message;
-  if (isError) {
-    statusElement.style.color = 'red';
-  } else {
-    statusElement.style.color = 'black';
-  }
+    statusElement.textContent = message;
+    statusElement.style.color = isError ? 'red' : 'black';
 }
+
 async function uploadFile(file) {
-  const formData = new FormData();
-  formData.append('file', file);
-  try {
-    const response = await axios.post(process.env.BACKEND_URL + '/upload', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
-    updateStatus(`Upload successful: ${response.data.message}`);
-  } catch (error) {
-    console.error('Error uploading file:', error);
-    updateStatus('Error uploading file. Please try again.', true);
-  }
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+        const response = await axios.post(`${process.env.BACKEND_URL}/upload`, formData, {
+            headers: { 'Content-Type': 'multipart/form-data' },
+        });
+        
+        updateStatus(`Upload successful: ${response.data.message}`);
+    } catch (error) {
+        console.error('Error uploading file:', error);
+        updateStatus('Error uploading file. Please try again.', true);
+    }
 }
+
 uploadButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  if (fileInput.files.length > 0) {
-    const file = fileInput.files[0];
-    updateStatus('Uploading file...');
-    uploadFile(file);
-  } else {
-    updateStatus('Please select a file to upload.', true);
-  }
+    e.preventDefault();
+    
+    if (fileInput.files.length > 0) {
+        const file = fileInput.files[0];
+        updateStatus('Uploading file...');
+        uploadFile(file);
+    } else {
+        updateStatus('Please select a file to upload.', true);
+    }
 });
