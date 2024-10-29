bdfc213e134a0b8cd6f42ed47a60376e5ca2dd46
Sci4rr <163331503+Sci4rr@users.noreply.github.com>
2024-10-29 01:11:56 +0100

    optimize the naming

--- components/FileUploadForm.js
+++ components/FileUploadForm.js
@@ -2,41 +2,46 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const FileUploadForm = () => {
-  const [selectedFile, setSelectedFile] = useState(null);
+    const [selectedFiles, setSelectedFiles] = useState([]);
 
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
+    const handleFileChange = (event) => {
+        setSelectedFiles(event.target.files);
+    };
 
-  const handleFileUpload = async () => {
-    if (!selectedFile) {
-      alert('Please select a file first.');
-      return;
-    }
+    const handleFileUpload = async () => {
+        if (!selectedFiles.length) {
+            alert('Please select one or more files first.');
+            return;
+        }
 
-    const formData = new FormData();
-    formData.append('file', selectedFile);
+        const uploadUrl = `${process.env.REACT_APP_API_URL}/YOUR_API_ENDPOINT`;
 
-    try {
-      const uploadUrl = `${process.env.REACT_APP_API_URL}/YOUR_API_ENDPOINT`;
-      const response = await axios.post(uploadUrl, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-      alert('File uploaded successfully');
-    } catch (error) {
-      console.error('Error uploading the file', error);
-      alert('Error uploading the file');
-    }
-  };
+        const uploadPromises = Array.from(selectedFiles).map(file => {
+            const formData = new FormData();
+            formData.append('file', file);
+            
+            return axios.post(uploadUrl, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+        });
 
-  return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleFileUpload}>Upload</button>
-    </div>
-  );
+        try {
+            await Promise.all(uploadPromises);
+            alert('All files uploaded successfully');
+        } catch (error) {
+            console.error('Error uploading files', error);
+            alert('Error uploading some or all files');
+        }
+    };
+
+    return (
+        <div>
+            <input type="file" onChange={handleFileChange} multiple />
+            <button onClick={handleFileUpload}>Upload</button>
+        </div>
+    );
 };
 
 export default FileUploadForm;
